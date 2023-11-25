3e0c1e7e34194da1f5c6bca96c0ef67de3c5d022
yuhexiong <yuhexiong871210@gmail.com>
2023-11-25 21:23:07 +0800

    fix: typo createDate comment

--- src/entity/type.ts
+++ src/entity/type.ts
@@ -6,7 +6,7 @@ export default class Type {
   @Column("varchar", { primary: true, comment: "id", length: 20, })
   id!: string;
 
-  @CreateDateColumn({ type: 'datetime', name: 'createDate', comment: '修改時間', })
+  @CreateDateColumn({ type: 'datetime', name: 'createDate', comment: '建立時間', })
   createDate!: Date;
 
   @Column({ type: "varchar", comment: "名稱", length: 20 })
